05ecdd265dd95441faaf963f9ac815daf616c4de
postsolar <120750161+postsolar@users.noreply.github.com>
2024-02-11 17:45:17 +0200

    Fix implementation of `Binding`'s `Bind` and `Apply` instances

--- src/AGS/Binding.js
+++ src/AGS/Binding.js
@@ -6,10 +6,30 @@ export const pureBinding =
   value =>
     Variable(value).bind("value")
 
+// TODO: FIXME: this might eventually get pushed upstream, remove it
+const combineBindings = (deps, fn) => {
+  const update = () => fn(...deps.map(d => d.transformFn(d.emitter[d.prop])))
+  const watcher = Variable(update())
+
+  for (const dep of deps)
+    dep.emitter.connect("changed", () => watcher.setValue(update()))
+
+  return watcher.bind()
+}
+
+export const applyBinding =
+  mf => ma =>
+    combineBindings([mf, ma], (f, a) => f(a))
+
 export const bindBinding =
-  binding => f =>
-    binding.transform(a => {
-      const { transformFn, emitter, prop } = f(a)
-      return transformFn(emitter[prop])
-    })
+  b1 => fn =>
+    { const b2 = fn(b1.transformFn(b1.emitter[b1.prop]))
+      const watcher = Variable(b2.emitter[b2.prop])
+      for (const b of [b1, b2])
+        b.emitter.connect("changed", () => {
+          const b2 = fn(b1.emitter[b1.prop])
+          watcher.setValue(b2.emitter[b2.prop])
+        })
+      return watcher.bind()
+    }
 
--- src/AGS/Binding.purs
+++ src/AGS/Binding.purs
@@ -7,17 +7,28 @@ import Untagged.Union (OneOf)
 
 foreign import data Binding ∷ Type → Type
 
+type role Binding representational
+
 type SelfOrBinding a = OneOf a (Binding a)
 
+-- | Transform the value streamed in a `Binding`.
 instance Functor Binding where
   map = transform
 
+-- | Combine multiple `Binding`s together.
+-- | A `Binding` produced with this instance will be updated whenever
+-- | *either* of the original bindings changes.
 instance Apply Binding where
-  apply = ap
+  -- Not derived from `bind` via `ap` because `applyBinding`
+  -- probably has better implementation
+  apply = applyBinding
 
 instance Applicative Binding where
   pure = pureBinding
 
+-- | Combine multiple `Binding`s together sequentially.
+-- | A `Binding` produced with this instance will be updated whenever
+-- | *either* of the original bindings changes.
 instance Bind Binding where
   bind = bindBinding
 
@@ -30,6 +41,7 @@ instance Monoid a ⇒ Monoid (Binding a) where
   mempty = pure mempty
 
 foreign import transform ∷ ∀ a b. (a → b) → Binding a → Binding b
+foreign import applyBinding ∷ ∀ a b. Binding (a → b) → Binding a → Binding b
 foreign import pureBinding ∷ ∀ a. a → Binding a
 foreign import bindBinding ∷ ∀ a b. Binding a → (a → Binding b) → Binding b
 
