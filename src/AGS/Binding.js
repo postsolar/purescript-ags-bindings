770aa5aed326c9a580e36231158c2cc32cc84db3
postsolar <120750161+postsolar@users.noreply.github.com>
2024-02-12 03:55:39 +0200

    Fix the lack of using `transformFn` in Binding.js
    Previously forgot to include it

--- src/AGS/Binding.js
+++ src/AGS/Binding.js
@@ -7,15 +7,15 @@ export const pureBinding =
     Variable(value).bind("value")
 
 // TODO: FIXME: this might eventually get pushed upstream, remove it
-const combineBindings = (deps, fn) => {
-  const update = () => fn(...deps.map(d => d.transformFn(d.emitter[d.prop])))
-  const watcher = Variable(update())
+const combineBindings = (deps, fn) =>
+  { const update = () => fn(...deps.map(d => d.transformFn(d.emitter[d.prop])))
+    const watcher = Variable(update())
 
-  for (const dep of deps)
-    dep.emitter.connect("changed", () => watcher.setValue(update()))
+    for (const dep of deps)
+      dep.emitter.connect("changed", () => watcher.setValue(update()))
 
-  return watcher.bind()
-}
+    return watcher.bind()
+  }
 
 export const applyBinding =
   mf => ma =>
@@ -24,11 +24,11 @@ export const applyBinding =
 export const bindBinding =
   b1 => fn =>
     { const b2 = fn(b1.transformFn(b1.emitter[b1.prop]))
-      const watcher = Variable(b2.emitter[b2.prop])
+      const watcher = Variable(b2.transformFn(b2.emitter[b2.prop0]))
       for (const b of [b1, b2])
         b.emitter.connect("changed", () => {
-          const b2 = fn(b1.emitter[b1.prop])
-          watcher.setValue(b2.emitter[b2.prop])
+          const b2 = fn(b1.transformFn(b1.emitter[b1.prop]))
+          watcher.setValue(b2.transformFn(b2.emitter[b2.prop]))
         })
       return watcher.bind()
     }
