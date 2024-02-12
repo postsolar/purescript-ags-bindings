08fa514a78c944a6e4390afd4e7ea736f6bb7c44
postsolar <120750161+postsolar@users.noreply.github.com>
2024-02-12 19:42:26 +0200

    Add `Overlay` widget

---
+++ src/AGS/Widget/Overlay.purs
@@ -0,0 +1,25 @@
+module AGS.Widget.Overlay
+  ( OverlayProps
+  , overlay
+  ) where
+
+import AGS.Widget (AGSWidgetProps, Widget)
+import Prelude ((<<<))
+import Prim.Row (class Union)
+import Type.Row (type (+))
+import Unsafe.Coerce (unsafeCoerce)
+
+type OverlayProps r =
+  AGSWidgetProps
+    +
+      ( child ∷ Widget
+      , overlays ∷ Array Widget
+      , passThrough ∷ Boolean
+      | r
+      )
+
+overlay ∷ ∀ r r'. Union r r' (OverlayProps ()) ⇒ Record r → Widget
+overlay = overlayImpl <<< unsafeCoerce
+
+foreign import overlayImpl ∷ Record (OverlayProps ()) → Widget
+
