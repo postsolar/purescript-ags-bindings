dd8252febdddc82f6c0148f830e0427b065e80c6
postsolar <120750161+postsolar@users.noreply.github.com>
2024-02-25 17:40:17 +0200

    Turn `GioFileMonitorEvent` into enum
    As enabled by #15

--- src/Gio/FileMonitorEvent.purs
+++ src/Gio/FileMonitorEvent.purs
@@ -1,16 +1,62 @@
 module Gio.FileMonitorEvent where
 
-foreign import data GioFileMonitorEvent ∷ Type
+import Data.Bounded (class Bounded)
+import Data.Bounded.Generic (genericBottom, genericTop)
+import Data.Enum (class BoundedEnum, class Enum)
+import Data.Enum.Generic
+  ( genericCardinality
+  , genericFromEnum
+  , genericPred
+  , genericSucc
+  , genericToEnum
+  )
+import Data.Eq (class Eq)
+import Data.Generic.Rep (class Generic)
+import Data.Ord (class Ord)
+import Data.Show (class Show)
+import Data.Show.Generic (genericShow)
 
-foreign import attributeChanged ∷ GioFileMonitorEvent
-foreign import changed ∷ GioFileMonitorEvent
-foreign import changesDoneHint ∷ GioFileMonitorEvent
-foreign import created ∷ GioFileMonitorEvent
-foreign import deleted ∷ GioFileMonitorEvent
-foreign import moved ∷ GioFileMonitorEvent
-foreign import movedIn ∷ GioFileMonitorEvent
-foreign import movedOut ∷ GioFileMonitorEvent
-foreign import preUnmount ∷ GioFileMonitorEvent
-foreign import renamed ∷ GioFileMonitorEvent
-foreign import unmounted ∷ GioFileMonitorEvent
+data GioFileMonitorEvent
+  -- A file changed.
+  = Changed
+  -- A hint that this was probably the last change in a set of changes.
+  | ChangesDoneHint
+  -- A file was deleted.
+  | Deleted
+  -- A file was created.
+  | Created
+  -- A file attribute was changed.
+  | AttributeChanged
+  -- The file location will soon be unmounted.
+  | PreUnmount
+  -- The file location was unmounted.
+  | Unmounted
+  -- The file was moved -- only sent if the (deprecated) Gio.FileMonitorFlags.SEND_MOVED flag is set
+  | Moved
+  -- The file was renamed within the current directory -- only sent if the Gio.FileMonitorFlags.WATCH_MOVES flag is set. Since: 2.46.
+  | Renamed
+  -- The file was moved into the monitored directory from another location -- only sent if the Gio.FileMonitorFlags.WATCH_MOVES flag is set. Since: 2.46.
+  | MovedIn
+  -- The file was moved out of the monitored directory to another location -- only sent if the Gio.FileMonitorFlags.WATCH_MOVES flag is set. Since: 2.46
+  | MovedOut
+
+derive instance Generic GioFileMonitorEvent _
+derive instance Eq GioFileMonitorEvent
+derive instance Ord GioFileMonitorEvent
+
+instance Show GioFileMonitorEvent where
+  show = genericShow
+
+instance Bounded GioFileMonitorEvent where
+  top = genericTop
+  bottom = genericBottom
+
+instance Enum GioFileMonitorEvent where
+  succ = genericSucc
+  pred = genericPred
+
+instance BoundedEnum GioFileMonitorEvent where
+  cardinality = genericCardinality
+  toEnum = genericToEnum
+  fromEnum = genericFromEnum
 
